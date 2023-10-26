69890258b766f4f505322e4d54a09df84ae8c185
Alelith <eacaymo@gmail.com>
2023-10-26 22:25:24 +0200

    Shoot animations

--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -16,12 +16,22 @@ public class PlayerShoot : MonoBehaviour
 
     //Types of shoot
     private float chargeTime = 0;
+    private bool isShooting = false;
+
+    //Animator
+    private Animator animator;
     #endregion
 
     #region Unity Functions
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
     private void Update()
     {
         GetInput();
+        ChangeAnimation();
     }
     #endregion
 
@@ -31,17 +41,31 @@ public class PlayerShoot : MonoBehaviour
         if (Input.GetMouseButton(0))
             chargeTime += Time.deltaTime;
         if (Input.GetMouseButtonUp(0))
-            Shoot();
+        {
+            StartCoroutine(Shoot());
+            chargeTime = 0;
+            isShooting = false;
+        }
     }
     #endregion
 
     #region Shoot Functions
-    private void Shoot()
+    private IEnumerator Shoot()
     {
+        isShooting = true;
+        yield return new WaitForSeconds(0.3f);
         if (chargeTime > 1)
             Instantiate(bullet, shootController.position, shootController.rotation).transform.localScale = new Vector3(2, 2, 2);
         else
             Instantiate(bullet, shootController.position, shootController.rotation);
+    }
+    #endregion
+
+    #region Animation Functions
+    private void ChangeAnimation()
+    {
+        animator.SetBool("isShooting", isShooting);
+        animator.SetFloat("charging", chargeTime);
     }
     #endregion
 }
